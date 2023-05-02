717cf14cc129c1dd1f55ce5f16dbf47495bdee90
sey2 <casio2978@naver.com>
2023-05-02 23:57:53 +0900

    Login CRUD AND JWT Init

---
+++ tryMetch/src/main/java/com/example/trymatch/security/entity/ClubMemberRole.java
@@ -0,0 +1,5 @@
+package com.example.trymatch.security.entity;
+
+public enum ClubMemberRole {
+    USER, MANAGER, ADMIN
+}
---
+++ tryMetch/src/test/java/com/example/trymatch/security/PasswordTests.java
@@ -0,0 +1,27 @@
+package com.example.trymatch.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class PasswordTests {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    //BCryptPasswordEncoder 동작 테스트용
+    @Test
+    public void testEncode(){
+        String password = "1111";
+
+        String enPw = passwordEncoder.encode(password);
+
+        System.out.println("enPw: " + enPw);
+
+        boolean mathResult = passwordEncoder.matches(password, enPw);
+
+        System.out.println("matchResult: " + mathResult);
+    }
+}
