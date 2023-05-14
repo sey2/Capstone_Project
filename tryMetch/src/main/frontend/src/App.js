c9c747b8d751a380c5e10f2102918570916ecb1b
sey2 <casio2978@naver.com>
2023-05-15 03:06:53 +0900

    kakao map init

--- tryMetch/src/main/frontend/src/App.js
+++ tryMetch/src/main/frontend/src/App.js
@@ -1,20 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import MapTest from './components/map/MapTest'
 
-function App() {
-  const [hello, setHello] = useState('')
+export default function App() {
+    const [hello, setHello] = useState('')
 
-  useEffect(() => {
-    axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-  }, []);
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(response => setHello(response.data))
+            .catch(error => console.log(error))
+    }, []);
 
-  return (
-      <div>
-        백엔드에서 가져온 데이터입니다 : {hello}
-      </div>
-  );
+
+    return (
+        <div>
+            <MapTest/>
+        </div>
+    );
 }
 
 export default App;
