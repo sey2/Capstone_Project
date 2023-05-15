938f4cad26857dfd83cc83537729c981ca52dae0
sey2 <casio2978@naver.com>
2023-05-16 03:41:17 +0900

    공공데이터 api 호출 기능 구현

--- tryMetch/src/main/frontend/src/components/map/MapTest.js
+++ tryMetch/src/main/frontend/src/components/map/MapTest.js
@@ -1,17 +1,24 @@
 /* global kakao */
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 const { kakao } = window;
 
 const MapTest = () => {
     const [map,setMap] = useState(null);
+    const [data, setData] = useState(null);
 
     //처음 지도 그리기
     useEffect(()=>{
         const container = document.getElementById('map');
         const options = { center: new kakao.maps.LatLng(33.450701, 126.570667) };
         const kakaoMap = new kakao.maps.Map(container, options);
+
         setMap(kakaoMap);
+
+        axios.get('/api/hello')
+            .then(response => setData(response.data))
+            .catch(error => console.log(error))
     },[])
 
     return (
---
+++ tryMetch/src/main/java/com/example/trymatch/travel/TravelServiceImpl.java
@@ -0,0 +1,2 @@
+package com.example.trymatch.travel;public class TravelServiceImpl {
+}
