4c25a005a3c367f546802d98ff28119a8d9c87eb
kimyongsu <akys159357@naver.com>
2023-05-21 17:00:45 +0900

    post create

---
+++ tryMetch/src/main/java/com/example/trymatch/board/PostController.java
@@ -0,0 +1,28 @@
+//package com.example.trymatch.board;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/posts")
+//public class PostController {
+//
+//
+//    private final PostRepository postRepository;
+//
+//    @Autowired
+//    public PostController(PostRepository postRepository) {
+//        this.postRepository = postRepository;
+//    }
+//
+//    //글 작성 페이지
+//    @GetMapping("/write")
+//    public String writeForm() {
+//        return "write_form";
+//    }
+//
+////    @PostMapping("/write")
+////    public String createPost()
+//}
---
+++ tryMetch/src/test/java/com/example/trymatch/board/createTest.java
@@ -0,0 +1,30 @@
+//package com.example.trymatch.board;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.time.LocalDateTime;
+//
+//@SpringBootTest
+//public class createTest {
+//
+//    @Autowired
+//    private PostRepository postRepository;
+//
+//    @Test
+//    void testPost(){
+//        Post post1 = new Post();
+//        post1.setId(null);
+//        post1.setTitle("테스트 제목1");
+//        post1.setContent("테스트 내용1");
+//        post1.setCreateDate(LocalDateTime.now());
+//
+//        Post post2 = new Post();
+//        post2.setId(null);
+//        post2.setTitle("테스트 제목2");
+//        post2.setContent("테스트 내용2");
+//        post2.setCreateDate(LocalDateTime.now());
+//    }
+//
+//}
