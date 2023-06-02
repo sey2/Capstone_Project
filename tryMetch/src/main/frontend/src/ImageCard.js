afd2923f7534dacf78c34feceeddc2fc01879c5c
YunYoung Choi <68066598+yunyoung0531@users.noreply.github.com>
2023-06-02 18:45:12 +0900

    Add files via upload

--- tryMetch/src/main/frontend/src/ImageCard.js
+++ tryMetch/src/main/frontend/src/ImageCard.js
@@ -1,12 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Spinner, Row, Col } from 'react-bootstrap';
 
 function ImageCard({ onImageClick }) {
     const [mapData, setMapData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [randomOrder, setRandomOrder] = useState([]);
 
     useEffect(() => {
         axios.get('/api/map')
@@ -17,27 +16,53 @@ function ImageCard({ onImageClick }) {
         .catch(error => console.log(error));
     }, []);
 
+    useEffect(() => {
+        if (!loading && mapData.length > 0) {
+        const order = generateRandomOrder(mapData.length);
+        setRandomOrder(order);
+        }
+    }, [loading, mapData]);
+
+    const generateRandomOrder = (length) => {
+        const order = [];
+        for (let i = 0; i < length; i++) {
+        order.push(i);
+        }
+        return shuffleArray(order);
+    };
+
+    const shuffleArray = (array) => {
+        const shuffled = [...array];
+        for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+        return shuffled;
+    };
+
     return (
         <div className="container mt-5 mb-5">
         <Row>
             {mapData.length > 0 && !loading ? (
-            mapData.map((data, index) => (
+            randomOrder.map((index) => {
+                const data = mapData[index];
+                return (
                 <Col key={index} md={4} sm={12} className="mb-4">
-                <div className="card card-img-brightness card-img-hover" onClick={() => onImageClick(data.city)}>
+                    <div className="card card-img-brightness card-img-hover" onClick={() => onImageClick(data.city)}>
                     <img src={data.img} className="rounded mx-auto d-block" alt="..." />
                     {data.city}
-                </div>
+                    </div>
                 </Col>
-            ))
+                );
+            })
             ) : (
-            <Spinner animation="border" role="status" className="spinner-display">
+                <Spinner animation="border" role="status" className="spinner-display">
                 <span className="visually-hidden spinner-display">Loading...</span>
-            </Spinner>
+                </Spinner>
             )}
         </Row>
         </div>
     );
-    }
+}
 
 export default ImageCard;
- 
