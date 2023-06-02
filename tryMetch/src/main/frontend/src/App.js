87b295e60cf91629da3dca063da00c979deb7238
yunyoung0531 <yunyoung0531@naver.com>
2023-06-02 15:59:44 +0900

    modified

--- tryMetch/src/main/frontend/src/App.js
+++ tryMetch/src/main/frontend/src/App.js
@@ -1,155 +1,126 @@
-//App.js
 import './App.css';
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Container, Nav} from 'react-bootstrap';
-import {Form} from 'react-bootstrap';
-import { data } from './data.js';
-import { Routes, Route, Link } from 'react-router-dom'
-import Detail from './Detail.js';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import MainCarousel from './MainCarousel';
 import { NavDropdown } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch} from '@fortawesome/free-solid-svg-icons'
 import CardList from './CardList';
-import { useNavigate } from 'react-router-dom'
 import More from './page/More';
 import NavSearch from './NavSearch';
-import ImageCard from './ImageCard';
 import MyPage from './page/MyPage';
-import axios from 'axios';
-import MapTest from './components/map/MapTest'
-import { useEffect } from 'react';
+import MapTest from './components/map/MapTest';
 import Login from './page/Login';
 
-
 function App() {
-  // let [shoes] = useState(data);
-  // console.log(shoes);
-  // let [shoes] = useState(data);
+  const navigate = useNavigate();
 
-  
-  const navigate = useNavigate()
+  const goToMyPage = () => {
+      navigate('/MyPage');
+  };
 
-  const goToMyPage = ()=> {
-      navigate("/MyPage")
-  }
-  const goToMore = ()=> {
-    navigate("/more")
-  }
+  const goToMore = () => {
+    navigate('/more');
+  };
 
-  const goToMapTest =()=>{
-    navigate("/MapTest")
-  }
+  const goToMapTest = () => {
+    navigate('/MapTest');
+  };
 
-  const goToLogin =()=>{
-    navigate("/Login")
-  }
+  const goToLogin = () => {
+    navigate('/Login');
+  };
+
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const showDropdown = () => {
+    setDropdownVisible(true);
+  };
+
+  const hideDropdown = () => {
+    setDropdownVisible(false);
+  };
 
   return (
-    <div className="App">
-
-{/* <div className='navbar-color'>
-        <Navbar variant="white">
-          <Container>
-            <Navbar.Brand href="#home">Shop</Navbar.Brand>
-            <Nav className="me-auto">
-            <Link to='/' className='link-home'>홈</Link>
-            <Link to='/detail' className='link-detail'>상세 페이지</Link>
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link> 
-            </Nav>
+      <div className="App">
+        <Navbar variant="white" expand="lg" bg="white" className="navbar-color">
+          <Container fluid className="navbar-color">
+            <div className="navbar-color">
+              <Navbar.Brand className="match-margin navbar-color">
+                <Link to="/" className="link-home">
+                  떠나 Match
+                </Link>
+              </Navbar.Brand>
+            </div>
+            <Navbar.Toggle aria-controls="navbar-dark-example" />
+            <Navbar.Collapse id="navbar-dark-example">
+              <Nav>
+                <NavDropdown
+                  id="nav-dropdown-dark-example"
+                  title=""
+                  menuVariant="dark"
+                  // show={dropdownVisible}
+                  // onMouseEnter={showDropdown}
+                  // onMouseLeave={hideDropdown}
+                >
+                  <NavDropdown.Item onClick={() => window.location.href = '/more'}>
+                    여행지 보기
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>여행 후기</NavDropdown.Item>
+                  <NavDropdown.Item>실시간 채팅</NavDropdown.Item>
+                  <NavDropdown.Item onClick={() => window.location.href = '/MapTest'}>
+                    주변 여행지 추천
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown onClick={goToMyPage}
+                    id="nav-dropdown-white-example"
+                    title={<span className="dropdown-text">마이페이지</span>}
+                    menuVariant="dark"
+                    show={dropdownVisible}
+                    onMouseEnter={showDropdown}
+                    onMouseLeave={hideDropdown}
+                  >
+                    <NavDropdown.Item onClick={() => window.location.href = '/Login'}>
+                      로그인
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => window.location.href = '/Login'}>
+                      로그아웃
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item onClick={() => window.location.href = '/Login'}>
+                      회원가입
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown.Divider />
+                </NavDropdown>
+                <Routes>
+                  <Route path="/" element={<NavSearch />} />
+                </Routes>
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
-      </div> */}
-
-      {/* ------------Navbar------------- */}
-      <Navbar variant="white" expand="lg" bg="white" className='navbar-color'>
-      <Container fluid className='navbar-color'>
-        <div className='navbar-color'><Navbar.Brand className='match-margin navbar-color'><Link to='/' className='link-home'>떠나 Match</Link></Navbar.Brand></div>
-        <Navbar.Toggle aria-controls="navbar-dark-example" />
-        <Navbar.Collapse id="navbar-dark-example">
-
-          <Nav>
-            <NavDropdown
-              id="nav-dropdown-dark-example"
-              title=""
-              menuVariant="dark"
-            >
-              <NavDropdown.Item href="" onClick={goToMore}>여행지 보기</NavDropdown.Item>
-              <NavDropdown.Item href="">
-                여행 후기
-              </NavDropdown.Item>
-              <NavDropdown.Item href="">실시간 채팅</NavDropdown.Item>
-              <NavDropdown.Item  onClick={goToMapTest}>주변 여행지 추천</NavDropdown.Item>
-              
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="" onClick={goToMyPage}>
-                마이페이지
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-                <NavDropdown.Item onClick={goToLogin}>로그인</NavDropdown.Item>
-                <NavDropdown.Item href="">로그아웃</NavDropdown.Item>
-            </NavDropdown>
-
-            {/* -------검색창------ */}
-            <Routes>
-            <Route path='/' element={<NavSearch/>} />
-            </Routes>
-
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-      {/* ------------------------------------------ */}
-
-      <Routes>
-        <Route 
-        path='/' element={<div>
-            <>
-              <div className='main-bg'><MainCarousel/></div>
-            </>  
-          {/* ------------------------------------------ */}
-
-        {/* -------카드리스트 세개와 더보기 -------------- */}
-          <CardList/>
-
-          <>
-                <div className='container'>
-                  <div className='row'>
-                    {
-                      
-                    }
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <>
+                  <div className="main-bg">
+                    <MainCarousel />
                   </div>
-                </div>
-          </>
-
-
-        </div>}/>
-        <Route path="/more" element={<More/>}/>
-        <Route path='/MyPage' element={<MyPage/>}/>
-        <Route path='/MapTest' element={<MapTest/>}/>
-        <Route path='/Login' element={<Login/>}/>
-      </Routes>
-
-
-
-
-    </div>
-  );
+                </>
+                <CardList />
+              </div>
+            }
+          />
+          <Route path="/more" element={<More />} />
+          <Route path="/MyPage" element={<MyPage />} />
+          <Route path="/MapTest" element={<MapTest />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </div>
+    );
 }
 
-// function Menu(props) {
-//   return (
-//     <div className="col-md-4">
-//       <img src={"" + props.im + ".jpg"} width="80%" />
-//       <h4>{props.shoes.title}</h4>
-//       <p>{props.shoes.content}</p>
-//       <p>{props.shoes.price}</p>
-//     </div>
-//   )
-// }
-
 export default App;
-
--- tryMetch/src/main/frontend/src/CardList.js
+++ tryMetch/src/main/frontend/src/CardList.js
@@ -1,67 +1,69 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretRight} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function CardList() {
+    const navigate = useNavigate();
 
-    const navigate = useNavigate()
-
-    const goToMore = ()=> {
-        navigate("/more")
+    const handleCardClick = (searchQuery, websiteUrl) => {
+        if (websiteUrl) {
+        window.open(websiteUrl, '_blank');
+        } else if (searchQuery) {
+        const encodedQuery = encodeURIComponent(searchQuery);
+        const naverSearchUrl = `https://search.naver.com/search.naver?query=${encodedQuery}`;
+        window.open(naverSearchUrl, '_blank');
+        }
     }
 
-    return(
+    const cards = [
+        {
+        imageSrc: 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200311_199%2F1583890188350cH60n_JPEG%2FewuwSU5L8pdZXSVJSEnc4oG6.JPG.jpg',
+        title: '전주 한옥마을',
+        websiteUrl: 'http://hanok.jeonju.go.kr/'
+        },
+        {
+        imageSrc: 'https://search.pstatic.net/common/?src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMzA0MTZfOTMg%2FMDAxNjgxNjAzODc3NzAz.7RLK_D5DFxJmc-yghipy0uV0TwVQx22uvIcf1SZZtLog.Fm5wbEuRBZfY9mITbqO_Avb6nXCRwMflxeHwEfGtaVMg.JPEG%2F72690F1B-1076-409A-AA46-141832E07C50.jpeg',
+        title: '담양 죽녹원',
+        websiteUrl: 'https://www.juknokwon.go.kr/index.juknok?menuCd=DOM_000000101001000000'
+        },
+        {
+        imageSrc: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MTdfMTE3%2FMDAxNjYwNzIwMzc3MTI4.7nwMKoQt0f7vvSeuDp90Z9CSBfp2gwwUJHTewA6MClgg.ILD1u8Y6pBTO2-2WZHv_W0hEMcy9hO_ENKVGaJRW5Rog.JPEG.586hot%2F20220815_105756.jpg',
+        title: '제주도 태웃개',
+        websiteUrl: ''
+        }
+    ];
 
-
-        <div className='container-list'>
-        <div class="container mt-5 mb-5">
-        <div class="row">
-
-        <>
-
-            <div class="col-md-3 col-sm-12">
-                <div class="Mcard">
-                    <img src="https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200311_199%2F1583890188350cH60n_JPEG%2FewuwSU5L8pdZXSVJSEnc4oG6.JPG.jpg" class="card-img-top" alt="..."/>
-                    <div class="card-body">
-                        <h5 class="card-text text-center Mcard-text">전주 한옥마을</h5>
-                    </div>
-                </div>
+    const renderCards = () => {
+        return cards.map((card, index) => (
+        <div className="col-md-3 col-sm-12" key={index}>
+            <div className="Mcard" onClick={() => handleCardClick(card.title, card.websiteUrl)}>
+            <img src={card.imageSrc} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-text text-center Mcard-text">{card.title}</h5>
             </div>
-    
-            <div class="col-md-3 col-sm-12">
-                <div class="Mcard">
-                    <img src="https://search.pstatic.net/common/?src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMzA0MTZfOTMg%2FMDAxNjgxNjAzODc3NzAz.7RLK_D5DFxJmc-yghipy0uV0TwVQx22uvIcf1SZZtLog.Fm5wbEuRBZfY9mITbqO_Avb6nXCRwMflxeHwEfGtaVMg.JPEG%2F72690F1B-1076-409A-AA46-141832E07C50.jpeg" class="card-img-top" alt="..."/>
-                    <div class="card-body">
-                        <h5 class="card-text text-center Mcard-text">담양 죽녹원</h5>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="col-md-3 col-sm-12">
-                <div class="Mcard">
-                    <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA4MTdfMTE3%2FMDAxNjYwNzIwMzc3MTI4.7nwMKoQt0f7vvSeuDp90Z9CSBfp2gwwUJHTewA6MClgg.ILD1u8Y6pBTO2-2WZHv_W0hEMcy9hO_ENKVGaJRW5Rog.JPEG.586hot%2F20220815_105756.jpg"  class="card-img-top" alt="..."/>
-                    <div class="card-body">
-                        <h5 class="card-text text-center Mcard-text">제주도 태웃개</h5>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-12">
-            <div className="moreBtn">
-            <Button className="moreBtn" onClick={goToMore}><FontAwesomeIcon icon={faCaretRight} beat size="2xl" style={{color: "ffffff",}} /></Button>
-
-            </div>
-            </div>
-            
-            </> 
-            
             </div>
         </div>
-    </div>
-    )
+        ));
+    }
+
+    return (
+        <div className="container-list">
+        <div className="container mt-5 mb-5">
+            <div className="row">
+            {renderCards()}
+            <div className="col-md-3 col-sm-12">
+                <div className="moreBtn">
+                <Button className="moreBtn" onClick={() => navigate("/more")}>
+                    <FontAwesomeIcon icon={faCaretRight} beat size="2xl" style={{ color: "ffffff" }} />
+                </Button>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    );
 }
 
 export default CardList;
