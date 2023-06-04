0efb30b5cbc82bf96a72c47ff71316de65f5cd84
kimyongsu <akys159357@naver.com>
2023-06-04 15:28:19 +0900

    error fetch

--- tryMetch/src/main/frontend/src/App.js
+++ tryMetch/src/main/frontend/src/App.js
@@ -26,64 +26,64 @@ import EditBoard from "./page/edit-board/EditBoard";
 
 
 function App() {
-  // const isNavVisible = window.location.pathname !== '/index';
+    // const isNavVisible = window.location.pathname !== '/index';
 
-  const location = useLocation();
+    const location = useLocation();
 
 
-  return (
-    <div className="App">
-      {/* {isNavVisible && <NavDrop />} */}
-      <NavDrop/>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <div className="main-bg">
-                <MainCarousel />
-              </div>
-              <CardList />
-            </div>
-          }
-        />
-        <Route path="/more" element={<More />} />
-        <Route path="/MyPage" element={<MyPage />} />
-        <Route path="/MapTest" element={<MapTest />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/board-list" element={<BoardList/>}/>
-        <Route
-          path="/add-board"
-          element={
-            <PrivateRoute path="/add-board" component={AddBoard}/>
-          }
-          />
+    return (
+        <div className="App">
+            {/* {isNavVisible && <NavDrop />} */}
+            <NavDrop/>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div>
+                            <div className="main-bg">
+                                <MainCarousel />
+                            </div>
+                            <CardList />
+                        </div>
+                    }
+                />
+                <Route path="/more" element={<More />} />
+                <Route path="/MyPage" element={<MyPage />} />
+                <Route path="/MapTest" element={<MapTest />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/board-list" element={<BoardList/>}/>
+                <Route
+                    path="/add-board"
+                    element={
+                        <PrivateRoute path="/add-board" component={AddBoard}/>
+                    }
+                />
 
-          <Route
-          path="/myboard-list"
-          element={
-            // 쿼리 파라미터가 존재하므로 전체 url을 PrivateRoute에 넘겨준다 `${location.pathname}`
-            <PrivateRoute path={`${location.pathname}`} component={MyBoardList}/>
-          }
-        />
-        <Route path="/board/:board_id" element={<Board/>}/>
-        <Route
-          path="/edit-board/:board_id"
-          element={
-            // URI 파라미터가 존재하므로 전체 url을 PrivateRoute에 넘겨준다
-            <PrivateRoute path={`${location.pathname}`} component={EditBoard}/>
-          }
-        />
+                <Route
+                    path="/myboard-list"
+                    element={
+                        // 쿼리 파라미터가 존재하므로 전체 url을 PrivateRoute에 넘겨준다 `${location.pathname}`
+                        <PrivateRoute path={`${location.pathname}`} component={MyBoardList}/>
+                    }
+                />
+                <Route path="/board/:board_id" element={<Board/>}/>
+                <Route
+                    path="/edit-board/:board_id"
+                    element={
+                        // URI 파라미터가 존재하므로 전체 url을 PrivateRoute에 넘겨준다
+                        <PrivateRoute path={`${location.pathname}`} component={EditBoard}/>
+                    }
+                />
 
-        <Route path="/chatgpt" element={<ChakraProvider theme={theme}>
-        <Chatgpt />
+                <Route path="/chatgpt" element={<ChakraProvider theme={theme}>
+                    <Chatgpt />
 
-       
-         </ChakraProvider> }/>
 
-      </Routes>
-    </div>
-  );
+                </ChakraProvider> }/>
+
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
