ffb0c3df93e499a1aa2c8138655e28651939b2c5
kimyongsu <akys159357@naver.com>
2023-06-05 21:03:10 +0900

    refactoring front board

--- tryMetch/src/main/frontend/src/page/add-board/AddBoard.js
+++ tryMetch/src/main/frontend/src/page/add-board/AddBoard.js
@@ -3,6 +3,7 @@ import {useNavigate} from "react-router-dom";
 import {useCallback, useState} from "react";
 import ImageUploader from "../../components/ImageUploader";
 import api from "../../utils/api";
+//import posts from "../../utils/posts";
 import {jwtUtils} from "../../utils/jwtUtils";
 import TextArea from "../../components/TextArea";
 import {Button} from "@mui/material";
@@ -11,38 +12,47 @@ import {toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 
- const AddBoard= () => {
+const AddBoard= () => {
     // 먼저 사용자 로그인 토큰을 확인 state.Auth.token
-     const token = useSelector(state => state.Auth.token); 
+    const token = useSelector(state => state.Auth.token);
     //const token = 1;
-    //const token = useSelector(state => 1 ); 
-    const navigate= useNavigate(); 
+    //const token = useSelector(state => 1 );
+    const navigate= useNavigate();
 
-    const [title, setTitle] = useState(""); 
-    const [content, setContent] = useState(""); 
-    const [image, setImage] = useState({
-        image_file: "", 
-        preview_URL: "image/default_image.jpg", 
-    }); 
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    // const [image, setImage] = useState({
+    //     image_file: "",
+    //     preview_URL: "image/default_image.jpg",
+    // });
+
+    // 이미지 파일 포함했을 때
+    // const canSubmit = useCallback(() => {
+    //      return image.image_file !== "" && content !== "" && title !== "";
+    // }, [image, title, content]);
 
     const canSubmit = useCallback(() => {
-        return image.image_file !== "" && content !== "" && title !== ""; 
-    }, [image, title, content]); 
+        return content !== "" && title !== "";
+    }, [title, content]);
+
 
     const handleSubmit = useCallback(async () => {
-      
+
         try {
 
-            const formData = new FormData(); 
-            formData.append("title", title); 
-            formData.append("content", content); 
-            formData.append("file", image.image_file); 
-            formData.append("user_id", jwtUtils.getId(token)); 
+            const formData = new FormData();
+            formData.append("title", title);
+            formData.append("content", content);
+            // formData.append("file", image.image_file);
+            formData.append("member_id", jwtUtils.getId(token));
 
-            await api.post("/api/board", formData); 
-            window.alert("등록이 완료되었습니다!"); 
+            // await api.post("/api/board", formData);
+
+            await api.post("/posts", formData);
+            window.alert("등록이 완료되었습니다!");
             navigate("/board-list");
-        }catch (e) {   
+        }catch (e) {
             toast.error("이모지 사용시 오류가 발생합니다!", {
                 position: "top-center",
             });
@@ -51,35 +61,35 @@ import "react-toastify/dist/ReactToastify.css";
 
     return (
         <div className="addBoard-wrapper">
-        <div className="addBoard-header">
-          여행 후기 작성하기 🖊️
+            <div className="addBoard-header">
+                여행 후기 작성하기 🖊️
+            </div>
+            <div className="submitButton">
+                {canSubmit() ? (
+                    <Button
+                        onClick={handleSubmit}
+                        className="success-button"
+                        variant="outlined"
+                    >
+                        게시물 등록하기
+
+                    </Button>
+                ) : (
+                    <Button
+                        className="disable-button"
+                        variant="outlined"
+                        size="large"
+                    >
+                        사진과 내용을 모두 입력하세요!
+                    </Button>
+                )}
+            </div>
+            <div className="addBoard-body">
+                {/* <ImageUploader setImage={setImage} preview_URL={image.preview_URL}/> */}
+                <TextArea setTitle={setTitle} setContent={setContent} title={title} content={content}/>
+            </div>
         </div>
-        <div className="submitButton">
-          {canSubmit() ? (
-            <Button
-              onClick={handleSubmit}
-              className="success-button"
-              variant="outlined"
-            >
-              게시물 등록하기
-        
-            </Button>
-          ) : (
-            <Button
-              className="disable-button"
-              variant="outlined"
-              size="large"
-            >
-              사진과 내용을 모두 입력하세요!
-            </Button>
-          )}
-        </div>
-        <div className="addBoard-body">
-          <ImageUploader setImage={setImage} preview_URL={image.preview_URL}/>
-          <TextArea setTitle={setTitle} setContent={setContent} title={title} content={content}/>
-        </div>
-      </div>
-      
+
     );
 
 }
--- tryMetch/src/main/frontend/src/page/board/Board.js
+++ tryMetch/src/main/frontend/src/page/board/Board.js
@@ -1,31 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom"
-import "./board.scss"; 
+import "./board.scss";
 import {Button, Dialog, DialogContent, IconButton} from "@mui/material";
-import {useSelector} from "react-redux"; 
+import {useSelector} from "react-redux";
 import api from "../../utils/api";
+//import posts from "../../utils/posts";
 import moment from "moment";
 import Comments from "../../components/Comments";
 import axios from "axios";
 import DisabledByDefaultOutlinedIcon from "@mui/icons-material/DisabledByDefaultOutlined";
 
 const Board =()=> {
-    const {board_id} = useParams(); 
-    const [board, setBoard] = useState({}); 
+
+    // const {board_id} = useParams();
+    // const [board, setBoard] = useState({});
+
+    const {post_id} = useParams();
+    const {posts, setBoard} = useParams({});
     const [isLoaded, setIsLoaded] = useState(false);
-    // token 
-     const token = useSelector(state => state.Auth.token);
-    //const token = 1;
-    //const token = useSelector(state => 1 );
-    const navigate = useNavigate(); 
-    
-    // Modal show 
-    const [show, setShow] = useState(false); 
-    // board 가져오기 
+
+    // token
+    // const token = useSelector(state => state.Auth.token);
+    const navigate = useNavigate();
+
+    // Modal show
+    const [show, setShow] = useState(false);
+
+    // board 가져오기
     useEffect(()=> {
         const getBoard = async ()=> {
-            const {data} = await axios.get(`/api/board/${board_id}`);
-            return data; 
+            const {data} = await axios.post(`/posts/${post_id}`);
+            return data;
         }
         getBoard().then(result => setBoard(result)).then(() => setIsLoaded(true));
     },[])
@@ -36,81 +41,85 @@ const Board =()=> {
             {isLoaded && (
                 <div className ="board-wrapper">
                     {
-                    <div className="edit-delete-button">
-                        <Button className="delete-button" onClick={() => {
-                            setShow(true)
-                        }}
-                        >삭제</Button>
+                        <div className="edit-delete-button">
+                            <Button className="delete-button" onClick={() => {
+                                setShow(true)
+                            }}
+                            >삭제</Button>
 
-                        <Button onClick={()=> {
-                            navigate(`/edit-board/${board_id}`)
-                        }}
-                        >수정</Button>
+                            <Button onClick={()=> {
+                                navigate(`/posts/${post_id}`)
+                            }}
+                            >수정</Button>
+                        </div>
+                    }
+
+                    {/* board-header,username,data, */}
+                    <div className="board-header">
+                        {/* <div className="board-header-username">{posts.user.username}</div> */}
+                        <div className="board-header-membername">{posts.member.membername}</div>
+                        <div className="board-header-date">{moment(posts.created).add(9, "hour").format('YYYY-MM-DD')}</div>
                     </div>
-            }
-
-            <div className="board-header">
-            <div className="board-header-username">{board.user.username}</div>
-            <div className="board-header-date">{moment(board.created).add(9, "hour").format('YYYY-MM-DD')}</div>
-          </div>
-          <hr/>
-          <div className="board-body">
-            <div className="board-image">
-              <img src={`/api/image/view/${board_id}`}/>
-            </div>
-            <div className="board-title-content">
-              <div className="board-title">{board.title}</div>
-              <div className="board-content">{board.content}</div>
-            </div>
-          </div>
-          <hr/>
-          <div className="board-footer">
-            <Comments board_id={board_id}/>
-          </div>
-        </div>
-      )}
-      {/*modal*/}
-      <Dialog open={show}>
-        <DialogContent style={{position: "relative"}}>
-          <IconButton
-            style={{position: "absolute", top: "0", right: "0"}}
-            onClick={() => setShow(false)}
-          >
-            <DisabledByDefaultOutlinedIcon/>
-          </IconButton>
-          <div className="modal">
-            <div className="modal-title"> 정말 삭제하시겠습니까?</div>
-            <div className="modal-button">
-              <Button
-                variant="outlined"
-                color="error"
-                onClick={async () => {
-                  setShow(false);
-                  // 모달의 예 버튼 클릭시 게시물 삭제
-                  await api.delete(`/api/board/${board_id}`);
-                  alert("게시물이 삭제되었습니다!");
-                  window.location.href = "/myboard-list";
-                }}
-              >
-                예
-              </Button>
-              <Button
-                variant="outlined"
-                color="primary"
-                onClick={() => {
-                  setShow(false)
-                }}
-              >
-                아니오
-              </Button>
-            </div>
-          </div>
-        </DialogContent>
-      </Dialog>
+                    <hr/>
+                    <div className="board-body">
+                        {/* ImageController 없는 상태 */}
+                        {/* <div className="board-image"> */}
+                        {/* <img src={`/api/image/view/${board_id}`}/> */}
+                        {/* </div> */}
+                        <div className="board-title-content">
+                            <div className="board-title">{posts.title}</div>
+                            <div className="board-content">{posts.content}</div>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="board-footer">
+                        {/* <Comments board_id={board_id}/> */}
+                        <Comments post_id={post_id}/>
+                    </div>
+                </div>
+            )}
+            {/*modal*/}
+            <Dialog open={show}>
+                <DialogContent style={{position: "relative"}}>
+                    <IconButton
+                        style={{position: "absolute", top: "0", right: "0"}}
+                        onClick={() => setShow(false)}
+                    >
+                        <DisabledByDefaultOutlinedIcon/>
+                    </IconButton>
+                    <div className="modal">
+                        <div className="modal-title"> 정말 삭제하시겠습니까?</div>
+                        <div className="modal-button">
+                            <Button
+                                variant="outlined"
+                                color="error"
+                                onClick={async () => {
+                                    setShow(false);
+                                    // 모달의 예 버튼 클릭시 게시물 삭제
+                                    await api.delete(`/posts/${post_id}`);
+                                    alert("게시물이 삭제되었습니다!");
+                                    window.location.href = "/myboard-list";
+                                }}
+                            >
+                                예
+                            </Button>
+                            <Button
+                                variant="outlined"
+                                color="primary"
+                                onClick={() => {
+                                    setShow(false)
+                                }}
+                            >
+                                아니오
+                            </Button>
+                        </div>
+                    </div>
+                </DialogContent>
+            </Dialog>
 
 
-                
-        
+
+
         </React.Fragment>
     );
 
